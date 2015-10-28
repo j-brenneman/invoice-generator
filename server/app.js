837576dd8718e74e6107a68969ea7d4bc6bdf3cc
Jacob Brenneman <j.brenneman19@gmail.com>
2015-10-28 14:12:09 -0600

    preparing for deploy

--- server/app.js
+++ server/app.js
@@ -46,6 +46,11 @@ if (app.get('env') === 'development') {
   });
 }
 
+// for use in production
+if(app.get('env') === 'production') {
+  app.listen(3000);
+}
+
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
